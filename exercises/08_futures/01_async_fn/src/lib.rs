93a89d8617f1874319f85bb25ba5307af10c3821
ultraman <1394466835@qq.com>
2024-11-16 16:36:40 +0800

    update

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -11,7 +11,16 @@ use tokio::net::TcpListener;
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        // Spawn a background task to handle the connection
+        // thus allowing the main task to immediately start
+        // accepting new connections
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            tokio::io::copy(&mut reader, &mut writer).await
+        });
+    }
 }
 
 #[cfg(test)]
--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -4,7 +4,27 @@ use tokio::net::TcpListener;
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    let handle1 = tokio::task::spawn(async move {
+        work(first).await;
+    });
+
+    let handle2 = tokio::task::spawn(async move {
+        work(second).await;
+    });
+
+    tokio::try_join!(handle1, handle2)?;
+
+    Ok(())
+}
+
+pub async fn work(ls: TcpListener) -> Result<(), anyhow::Error> {
+    loop {
+        let (mut socket, _) = ls.accept().await?;
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            tokio::io::copy(&mut reader, &mut writer).await
+        });
+    }
 }
 
 #[cfg(test)]
